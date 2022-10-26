1eaafa28fb66eab91fbd8a78ed122fd3a98ca2e0
r7qb <r7qb@fresnou.org>
2022-10-26 11:17:22 -0700

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const toDoController = require("../controllers/todo_controller.js")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', toDoController.homeRoute);
 
+router.get('/add', toDoController.renderAddForm)
+
+router.post('/add', toDoController.addNewItems)
+
+router.get('/complete/:id', toDoController.markItemsAsComplete)
+
+router.get('/incomplete/:id', toDoController.markItemsAsIncomplete)
+
+router.get('/delete/:id', toDoController.deleteItem)
+
+router.get('/edit/:id', toDoController.renderEditForm)
+
+router.post('/edit/:id', toDoController.updateItem)
 module.exports = router;
